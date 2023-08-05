dd1c672f67a949861b5a5be661254e24039e922b
Vencislav Atanasov <git@6bez10.info>
2023-08-05 22:00:47 +0300

    Add JSON parser and CORS middlewares

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,6 +1,10 @@
-import Router from 'express';
+import Router, { json } from 'express';
 import { getDoors } from './doors.js';
+import cors from 'cors';
 
 export const apiRouter = new Router();
+
+apiRouter.use(json());
+apiRouter.use(cors());
 
 apiRouter.get('/doors', getDoors);
