0b990d8433b497d8b2625efee07b6b7fb8515953
Vencislav Atanasov <git@6bez10.info>
2023-08-05 21:52:57 +0300

    Remove example route

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
 import express from 'express';
-import { createDebug } from './debug.js';
 import { apiRouter } from './routes/api/index.js';
 
-const debug = createDebug('express');
 export const app = express();
-
-app.get('/', (req, res) => {
-    debug('request');
-    res.send('Hello World!');
-});
 
 app.use('/api', apiRouter);
