76a56c22b1d55cf5cb4fa18e466f8833a243d806
Vencislav Atanasov <git@6bez10.info>
2023-08-27 20:29:05 +0300

    Remove dummy getUser function

--- src/user.js
+++ src/user.js
@@ -1,12 +1,3 @@
-export function getUser() {
-    // TODO
-
-    return {
-        language: 'en',
-        roles: ['trusted_member'],
-    };
-}
-
 export function isAuthorised(userRoles, resourceRoles) {
     return resourceRoles.reduce((prev, curr) => prev || userRoles.includes(curr), false);
 }
