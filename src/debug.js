8b8f1713bba44a7d637356c8d4152929622631c1
Vencislav Atanasov <git@6bez10.info>
2023-10-14 02:34:24 +0300

    Fix logging in debug mode

--- src/debug.js
+++ src/debug.js
@@ -1,4 +1,13 @@
 import factory from 'debug';
+import util from 'node:util';
+
+function logToConsole(...args) {
+    return console.log(util.format(...args));
+}
+
+if ((process.env.NODE_OPTIONS || '').endsWith('/debugConnector.js')) {
+    factory.log = logToConsole;
+}
 
 const debug = factory('app');
 
