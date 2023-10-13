f491a1b9f0c7f265fc4e483d1ab011f24c55db87
Vencislav Atanasov <git@6bez10.info>
2023-10-14 02:54:26 +0300

    Fix upsert API to return model

--- src/database/api.js
+++ src/database/api.js
@@ -5,11 +5,13 @@ const findByPk = async (entity, id, fatal = true) => entity.findByPk(id, {
     rejectOnEmpty: fatal && new EmptyResultError('Entity not found: ' + entity.name),
 });
 
+const upsert = async (entity, fields) => (await entity.upsert(fields))[0];
+
 export const getApplication = async id => findByPk(Application, id);
-export const createOrUpdateApplication = async (fields = {}) => Application.upsert(fields);
+export const createOrUpdateApplication = async (fields = {}) => upsert(Application, fields);
 
 export const getUser = async id => findByPk(User, id);
-export const createOrUpdateUser = async (fields = {}) => User.upsert(fields);
+export const createOrUpdateUser = async (fields = {}) => upsert(User, fields);
 
 export const createActionLog = async (fields = {}) => ActionLog.create(fields);
 export const getActionLogs = async () => ActionLog.findAll();
