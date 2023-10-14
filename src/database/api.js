4c79675580e91305dac7c8d298f0cfcc9fa3c220
Vencislav Atanasov <git@6bez10.info>
2023-10-14 05:28:58 +0300

    Remove unused database APIs

--- src/database/api.js
+++ src/database/api.js
@@ -1,16 +1,9 @@
-import { EmptyResultError } from 'sequelize';
 import { ActionLog, Application, User } from './index.js';
-
-const findByPk = async (entity, id, fatal = true) => entity.findByPk(id, {
-    rejectOnEmpty: fatal && new EmptyResultError('Entity not found: ' + entity.name),
-});
 
 const upsert = async (entity, fields) => (await entity.upsert(fields))[0];
 
-export const getApplication = async id => findByPk(Application, id);
 export const createOrUpdateApplication = async (fields = {}) => upsert(Application, fields);
 
-export const getUser = async id => findByPk(User, id);
 export const createOrUpdateUser = async (fields = {}) => upsert(User, fields);
 
 export const createActionLog = async (fields = {}) => ActionLog.create(fields);
