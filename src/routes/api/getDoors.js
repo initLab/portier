3f1821e27cc20ad5e85492a6d587c9b149d95935
Vencislav Atanasov <git@6bez10.info>
2023-10-14 04:06:05 +0300

    Add logging to getDoors

--- src/routes/api/getDoors.js
+++ src/routes/api/getDoors.js
@@ -1,9 +1,14 @@
 import { listUserAccessibleDoors } from '../../doors.js';
+import { createDebug } from '../../debug.js';
+
+const debug = createDebug('getDoors');
 
 export function getDoors(req, res) {
     if (!req.user) {
+        debug('Unauthenticated');
         return res.status(403).end();
     }
 
+    debug('Successful');
     res.json(listUserAccessibleDoors(req.user));
 }
