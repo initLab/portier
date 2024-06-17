e4171cf538703bd702abb76ac1e66a45428a9c19
Vencislav Atanasov <git@6bez10.info>
2024-06-18 00:12:12 +0300

    Add passport profile config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,17 @@
 import config from '../config.json' assert { type: 'json' };
+import passportProfile from '../passport-profile.json' assert { type: 'json' };
 
 config.devices = Object.entries(config?.devices || {}).map(([id, device]) => ({
     id,
     ...device,
 }));
+
+config.passport = {
+    ...config.passport,
+    authorization: {
+        ...config.passport.authorization,
+        profile: passportProfile,
+    },
+};
 
 export { config };
